a583be38c5a85502d2bfa9b43fc670cf826129b8
ahmet-kargin <96681639+ahmet-kargin@users.noreply.github.com>
2022-05-12 20:02:51 +0300

    12.05.2022

--- Otobus_Proje/script.js
+++ Otobus_Proje/script.js
@@ -8,3 +8,21 @@ $(document).ready(function () {
     })
         
 })
+
+const tikla = document.querySelector(".UserLogin");
+const mainPopup = document.querySelector(".main-popup");
+const close = document.querySelector(".close-popup");
+
+tikla.addEventListener("click",()=>{
+    mainPopup.style.display="block";
+})
+
+// close.addEventListener("click",()=>{
+//     mainPopup.style.display="none";
+// })
+
+mainPopup.addEventListener("click", e =>{
+    if(e.target.className=="main-popup" || e.target.className=="close-popup"){
+        mainPopup.style.display="none";
+    }
+})
